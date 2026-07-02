c1029cbee4073a43d8c6f5ad1002880d9080c276
Morgan Powers <morgan.c.powers@gmail.com>
2026-07-01 19:35:42 -0500

    fix memory allocation

--- runner.c
+++ runner.c
@@ -5,17 +5,19 @@
 
 int main() {
     u_int32_t* machine_code = parse("bin/nopret.s");
+    size_t MC_SIZE = sizeof(u_int32_t)*current_size;
 
     // Allocate memory page
-    void *mem = mmap(NULL, sizeof(*machine_code), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON | MAP_JIT, -1, 0);
+    void *mem = mmap(NULL, MC_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON | MAP_JIT, -1, 0);
 
     // Copy bytes to the executable memory space
-    memcpy(mem, machine_code, sizeof(u_int32_t)*current_size);
+    memcpy(mem, machine_code, MC_SIZE);
+
     //clear instruction cache
-    __builtin___clear_cache(mem, (char *)mem + sizeof(*machine_code));
+    __builtin___clear_cache(mem, (char *)mem + MC_SIZE);
 
     //change allocated memory to have Execute permissions
-    if (mprotect(mem, sizeof(machine_code), PROT_READ | PROT_EXEC) != 0) perror("mprotect");
+    if (mprotect(mem, MC_SIZE, PROT_READ | PROT_EXEC) != 0) perror("mprotect");
 
     // Cast memory address to a function pointer and execute it
     void (*func)(void) = mem;
