e68c13a601a323859be4cb8e4ae839a12805736e
Morgan Powers <morgan.c.powers@gmail.com>
2026-07-03 13:48:49 -0500

    documentation + refactoring

--- runner.c
+++ runner.c
@@ -7,7 +7,8 @@ int main(int argc, char *argv[]) {
     u_int32_t* machine_code = parse(argv[1]);
     size_t MC_SIZE = sizeof(u_int32_t)*current_size;
 
-    // Allocate memory page
+    // Allocate isolated memory page for rwx in JIT
+    //       starting addr, size,        read | write, local to process | unshared | allows toggle between w,x via JIT
     void *mem = mmap(NULL, MC_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANON | MAP_JIT, -1, 0);
 
     // Copy bytes to the executable memory space
@@ -24,6 +25,9 @@ int main(int argc, char *argv[]) {
     int r = func();
     printf("Return code: %d\n", r);
 
+    //free allocated memory
     free(machine_code);
+    if(munmap(mem,MC_SIZE)!=0) perror("munmap");
+
     return 0;
 }
