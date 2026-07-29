50ca8aed4380f1e3d1e385c054aed7e44f6a75c9
Morgan Powers <morgan.c.powers@gmail.com>
2026-07-28 19:24:07 -0500

    binary parser

--- parser.c
+++ parser.c
@@ -1,5 +1,14 @@
 #include "parser.h"
 
+void print_binary(uint32_t code) {
+    printf("0b");
+    for (int i = 31; i >= 0; i--) {
+        uint32_t bit = (code >> i) & 1;
+        printf("%u", bit);
+    }
+    printf("\n");
+}
+
 char peek(FILE *input){
     char c = getc(input);
     ungetc(c,input);
@@ -11,9 +20,20 @@ void skipLine(FILE* input){
   while(c != '\n' && c != EOF) c= getc(input);
 }
 
+const char* get_file_extension(const char *fpath) {
+    // Find the last occurrence of '.'
+    const char *dot = strchr(fpath, '.');
+
+    // If dot is not found, or it is the first character (hidden file like .htaccess), return empty string
+    if (!dot || dot == fpath) return "";
+
+    // Return the extension (characters after the dot)
+    return dot;
+}
+
 int current_size=0;
 
-u_int32_t* parse(char* fpath){
+u_int32_t* parse_hex(char* fpath){
     //open as binary
     FILE *file = fopen(fpath, "r");
     if (file == NULL) perror("Error opening file");
@@ -45,4 +65,45 @@ u_int32_t* parse(char* fpath){
 
     fclose(file);
     return machine_code;
+}
+
+u_int32_t parse_binary_op(FILE* file){
+    u_int32_t code = 0b00000000000000000000000000000000;
+
+    for(int i=31; i>=0; i--){
+        char c = peek(file);
+        if(isspace(c)||(c==EOF)||(c=='\n')||(c=='#')) return code;
+        switch(getc(file)){
+            case '1': code |= (1<<i);break;
+            case '0': break;
+            default: perror("Non-binary character");
+        }
+    }
+    return code;
+}
+u_int32_t* parse_binary(char *fpath){
+    FILE *file = fopen(fpath, "r");
+    if (file == NULL) perror("Error opening file");
+
+    current_size = 0;
+    u_int32_t* machine_code = malloc(sizeof(u_int32_t)*current_size);
+
+    char c = peek(file);
+    while(c!=EOF){
+        if(c=='#') skipLine(file);//comment
+        u_int32_t code = parse_binary_op(file);
+
+        current_size++;
+        machine_code = realloc(machine_code, current_size * sizeof(u_int32_t));
+        machine_code[current_size-1] = code;
+
+        print_binary(code);
+        c = peek(file);
+    }
+    return machine_code;
+}
+
+u_int32_t* parse(char* fpath){
+    if(strcmp(get_file_extension(fpath),".bin.s")==0) return parse_binary(fpath);
+    return parse_hex(fpath);
 }
