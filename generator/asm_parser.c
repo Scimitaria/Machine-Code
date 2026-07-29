d590829f9ad8a89685063605792c9da319b99f69
Morgan Powers <morgan.c.powers@gmail.com>
2026-07-29 09:01:25 -0500

    update gen args

--- generator/asm_parser.c
+++ generator/asm_parser.c
@@ -1,6 +1,11 @@
 #include "generator.h"
 
-int main(){
-    print_hex(ret);
+int main(int argc, char *argv[]){
+    //manual generation goes here
+
+    if(argc!=0){
+        char* fpath = argv[0];
+        parse(fpath);
+    }
     return 0;
 }
